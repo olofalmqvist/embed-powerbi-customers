659e0e1b440d7c747ff48f52e3018b1015576a5d
Olof Almqvist <Olofal@hotmail.com>
2022-04-03 22:58:58 +0200

    initial: initial commit

---
+++ AppOwnsData/Contracts/AuthenticatorMode.cs
@@ -0,0 +1,8 @@
+﻿namespace AppOwnsData.Contracts
+{
+    public static class AuthenticatorMode
+    {
+        public static string ServicePrincipal = "ServicePrincipal";
+        public static string MasterUser = "MasterUser";
+    }
+}
